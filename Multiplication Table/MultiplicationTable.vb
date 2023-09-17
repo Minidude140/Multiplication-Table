54fe3a403a998db28dd065572875af3d1e1574bd
Zac <zac.minidude140@Gmail.com>
2023-09-17 00:21:41 -0600

    Add project files.

---
+++ Multiplication Table/MultiplicationTable.vb
@@ -0,0 +1,17 @@
+﻿'Zachary Christensen
+'RCET 2265
+'Fall 2023
+'Multiplication Table
+'https://github.com/Minidude140
+
+
+Option Explicit On
+Option Strict On
+
+Module MultiplicationTable
+
+    Sub Main()
+
+    End Sub
+
+End Module
