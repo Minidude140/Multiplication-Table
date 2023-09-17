1594cfab598b04fea849e0ada85a3bd244074d99
Zac <zac.minidude140@Gmail.com>
2023-09-17 00:25:50 -0600

    Added git link in header and TODO list

--- Multiplication Table/MultiplicationTable.vb
+++ Multiplication Table/MultiplicationTable.vb
@@ -2,13 +2,18 @@
 'RCET 2265
 'Fall 2023
 'Multiplication Table
-'https://github.com/Minidude140
+'https://github.com/Minidude140/Multiplication-Table.git
 
 
 Option Explicit On
 Option Strict On
 
 Module MultiplicationTable
+    'TODO
+    '[]Ask user for size of multiplication table
+    '[]Create Array at desired size
+    '[]Populate Array with multiplied data
+    '[]Write to the console the table with proper formated rows and columns
 
     Sub Main()
 
